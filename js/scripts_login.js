4441237a7e6ae5b239218af56e21e9bad11a518c
Jhony Almeida <jhony12358oficial@gmail.com>
2022-09-01 14:11:33 -0300

    Ativade Online 3

---
+++ js/scripts_login.js
@@ -0,0 +1,11 @@
+function log() {
+    let log = document.getElementById("log").value
+    alert("Você logou com Suceesso!!!!!")
+    console.log("Você logou com Suceesso!!!!!")
+}
+
+function esquecer_senha() {
+    let esquecer_senha = document.getElementById("esquecer-senha").value
+    alert("Infelizmente não adicionamos nenhuma função para recuperar sua senha :(")
+    console.log("Infelizmente não adicionamos nenhuma função para recuperar sua senha :(")
+}
